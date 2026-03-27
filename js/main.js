156ad667eea2fbc97a69d9eb8d936c779f36b6ac
samudoria <samueledoria@gmail.com>
2026-03-27 10:42:01 +0100

    cv and speed

--- js/main.js
+++ js/main.js
@@ -59,29 +59,41 @@ contact.addEventListener("click", () => {
 // Typewriter.js
 // https://github.com/ronv/Typewriter.js
 
-$.fn.typewriter = function () {
-    this.each(function () {
-        var c = $(this),
-            b = c.html(),
-            a = 0,
-            d = 0;
-        c.html("");
-        var e = function () {
-            if ("<" == b.substring(a, a + 1)) {
-                var f = new RegExp(/<span class="instant"/),
-                    g = new RegExp(/<span class="clear"/);
-                if (b.substring(a, b.length).match(f))
-                    a += b.substring(a, b.length).indexOf("</span>") + 7;
-                else if (b.substring(a, b.length).match(g))
-                    (d = a),
-                        (a += b.substring(a, b.length).indexOf("</span>") + 7);
-                else for (; ">" != b.substring(a, a + 1);) a++;
+const TYPEWRITER_MAX_DELAY_MS = 7;
+
+function applyTypewriter(element) {
+    let content = element.innerHTML;
+    let index = 0;
+    let clearIndex = 0;
+
+    element.innerHTML = "";
+
+    const step = () => {
+        if (content.substring(index, index + 1) === "<") {
+            const remaining = content.substring(index);
+            const hasInstantSpan = /<span class="instant"/.test(remaining);
+            const hasClearSpan = /<span class="clear"/.test(remaining);
+
+            if (hasInstantSpan) {
+                index += remaining.indexOf("</span>") + 7;
+            } else if (hasClearSpan) {
+                clearIndex = index;
+                index += remaining.indexOf("</span>") + 7;
+            } else {
+                while (content.substring(index, index + 1) !== ">") {
+                    index++;
+                }
             }
-            c.html(b.substring(d, a++) + (a & 1 ? "_" : ""));
-            a >= b.length || setTimeout(e, 15 * Math.random());
-        };
-        e();
-    });
-    return this;
-};
-$(".terminal").typewriter();
+        }
+
+        element.innerHTML = content.substring(clearIndex, index++) + (index & 1 ? "_" : "");
+
+        if (index < content.length) {
+            setTimeout(step, TYPEWRITER_MAX_DELAY_MS * Math.random());
+        }
+    };
+
+    step();
+}
+
+document.querySelectorAll(".terminal").forEach(applyTypewriter);
